9555c7c92aeac7a2173e78e24021d3858d5fbb00
mihkel tanel <mihkel.tanel@gmail.com>
2019-02-11 15:40:01 +0200

    changed menu and added minimize to tray option

--- main.js
+++ main.js
@@ -1,46 +1,126 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Tray, Menu, ipcMain } = require('electron')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let win
-
-function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
-
-  // and load the index.html of the app.
-  win.loadFile('index.html')
-
-  // Open the DevTools.
-  win.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null
-  })
-}
+let mainWindow;
+//app icon path
+const iconpath = ('./icon.png')
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+//create new MainWindow on app open 
+function createWindow() {
+    mainWindow = new BrowserWindow({ width: 800, height: 600, icon: iconpath })
 
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
-  }
-})
+
+    // and load the index.html of the app.
+    mainWindow.loadFile('index.html')
+
+
+
+    //quit app on close
+    mainWindow.on('close', function () {
+        app.quit();
+    })
+    //minimizes to tray
+    mainWindow.on('minimize', function (event) {
+        event.preventDefault()
+        mainWindow.hide()
+    })
+
+    mainWindow.on('show', function () {
+        appIcon.setHighlightMode('always')
+    })
+
+    const appIcon = new Tray(iconpath)
+    //app settings when minimzed
+    const contextMenu = Menu.buildFromTemplate([
+        {
+            label: 'Show App', click: function () {
+                mainWindow.show()
+            }
+        },
+        {
+            label: 'Quit', click: function () {
+                app.isQuiting = true
+                app.quit()
+            }
+        },
+
+
+
+    ])
+    //open from tray when double clicked the icon
+    appIcon.on('double-click', () => {
+        mainWindow.show()
+    })
+    appIcon.setContextMenu(contextMenu)
+
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    // Insert menu
+    Menu.setApplicationMenu(mainMenu);
+
+}
+
+
+const mainMenuTemplate = [
+    // Each object is a dropdown
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Settings',
+                click() {
+                    createSettingsWindow();
+                }
+            },
+            {
+                label: 'Quit',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
+
+// If OSX, add empty object to menu
+if (process.platform == 'darwin') {
+    mainMenuTemplate.unshift({});
+}
+
+// Add developer tools option if in dev
+if (process.env.NODE_ENV !== 'production') {
+    mainMenuTemplate.push({
+        label: 'Developer Tools',
+        submenu: [
+            {
+                role: 'reload'
+            },
+            {
+                label: 'Toggle DevTools',
+                accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            }
+        ]
+    });
+}
+
+
+function createSettingsWindow() {
+    settingsWindow = new BrowserWindow({ width: 300, height: 200, title: "settings", frame: false })
+    settingsWindow.loadFile('settings.html')
+    // Handle garbage collection
+    settingsWindow.on('close', function () {
+        settingsWindow = null;
+    });
+};
+//Catches settingsAdd and changes the settings -> closes window
+ipcMain.on('settingsSave', function(e, msg){
+    console.log(msg)
+    settingsWindow.close(); 
+
+  });
