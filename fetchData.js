f539828a354a915a73fe21fcce6d7c059f14aa24
mihkel tanel <mihkel.tanel@gmail.com>
2019-02-15 18:17:20 +0200

    added refresh option under file, settings function and styled settings windows

--- fetchData.js
+++ fetchData.js
@@ -6,7 +6,8 @@ const { ipcRenderer } = require('electron')
 let first = true;
 // how often the data is updated in milliseconds
 let updateFrequency = 300000;
-
+let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
+var interval;
 
 //Fetch data and return json
 async function getData(url) {
@@ -16,27 +17,26 @@ async function getData(url) {
 }
 //call for fetch and print data
 async function main() {
-    //const data = await getData("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson");
-    //const data = await getData("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson");
-    //const data = await getData("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson");
-    const data = await getData("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson");
-    
+
+
+    const data = await getData(url);
+    console.log("printing data")
     //print table behinning
     const tableBeginning = "<table class='table table-striped table-hover' id='EQList'><tr><td scope='col'>Magnitude</td><td scope='col'>Location</td><td scope='col'>Time </td></tr>"
     $(tableInsert).append(tableBeginning);
     //print all data inside the table
     data.features.forEach(function (item) {
         const coords = item.geometry.coordinates;
-        const id = item.id; 
+        const id = item.id;
         const magnitude = "<td>" + item.properties.mag + "</td>";
         const DateOptions = { hour: 'numeric', minute: 'numeric', seconds: 'numeric', year: 'numeric', month: 'numeric', day: 'numeric' }
         const timeFormatted = new Date(item.properties.time).toLocaleDateString("en-GB", DateOptions);
-        const time = "<td>" + timeFormatted+"</td>";
+        const time = "<td>" + timeFormatted + "</td>";
         const location = "<td>" + item.properties.place + "</td>";
-        let start = "<tr onclick='test(\""+coords+"\")'>";
+        let start = "<tr onclick='test(\"" + coords + "\")'>";
         const end = "</tr>";
         //check if this message is new, if it is throw a popup with earthquake data
-        checkIfIdExists(id, item.properties.mag , item.properties.place, timeFormatted);
+        checkIfIdExists(id, item.properties.mag, item.properties.place, timeFormatted);
         //Change the color of line to orange if the magnitude is over 7 and red if it's over 8
         if (item.properties.mag > 7) {
             start = "<tr style=color:orange>";
@@ -47,38 +47,68 @@ async function main() {
         const msg = start + magnitude + location + time + end;
         //apend msg to table
         $(EQList).append(msg);
+    
     });
     //print table ending
     const tableEnd = "</table>";
     $(EQList).append(tableEnd);
+    first = false;
     //print last updated at the end of the table
-    $(lastUpdate).html("Last update: "+new Date());
+    $(lastUpdate).html("Last update: " + new Date());
 }
 
-function checkIfIdExists(id,mag,loc,time) {
+function checkIfIdExists(id, mag, loc, time) {
     if (!store.has(id) && first === false) {
-        msg = "Magnitude "+mag+" earthquake @ "+loc+"  "+time;
+        msg = "Magnitude " + mag + " earthquake @ " + loc + "  " + time;
         //tray popup
         ipcRenderer.send('openNotification', msg);
-        store.set(id,id);
+        store.set(id, id);
 
-    } else{
+    } else {
 
-        store.set(id,id);
+        store.set(id, id);
 
     }
 
- 
+
 }
+loadDatafromStorage();
 main()
-function clearData(){
+
+function clearData() {
     $(tableInsert).empty();
     main();
-    first = false;
+    
+    
 }
 
-function test(msg){
+function test(msg) {
     ipcRenderer.send('openMap', msg);
 }
 
-setInterval(function(){ clearData() }, updateFrequency);
+
+
+ipcRenderer.on('update', function (e, ) {
+    first = true;
+    loadDatafromStorage()
+    clearData();
+
+});
+
+//load settings data
+function loadDatafromStorage() {
+    //set updatefrequency from local storage
+    console.log("loading data from storage current freq = " + updateFrequency + " and url = " + url)
+    if (updateFrequency != store.get("frequency")) {
+        updateFrequency = parseInt(store.get("frequency"));
+        clearInterval(interval);
+        
+    }
+    //set fetch url from local storage
+    if (store.has("url")) {
+        url = store.get("url")
+    }
+    interval = setInterval(function () { clearData() }, updateFrequency);
+    console.log("loaded data, new values = " + updateFrequency + " and url = " + url)
+
+}
--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const { app, BrowserWindow, Tray, Menu, ipcMain } = require('electron')
 let mainWindow;
 let settingsWindow;
 let mapWindow;
+const Store = require('electron-store');
+const store = new Store();
 //app icon path
 const iconpath = ('./icon.png')
 let tray;
@@ -76,6 +78,12 @@ const mainMenuTemplate = [
         label: 'File',
         submenu: [
             {
+                label: 'Refresh',
+                click() {
+                    mainWindow.reload()
+                }
+            },
+            {
                 label: 'Settings',
                 click() {
                     createSettingsWindow();
@@ -118,7 +126,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 
 function createSettingsWindow() {
-    settingsWindow = new BrowserWindow({ width: 300, height: 200, title: "settings", frame: false })
+    settingsWindow = new BrowserWindow({ width: 300, height: 300, title: "settings", frame: false })
     settingsWindow.loadFile('settings.html')
     // Handle garbage collection
     settingsWindow.on('close', function () {
@@ -126,7 +134,8 @@ function createSettingsWindow() {
     });
 };
 function createMapWindow() {
-    mapWindow = new BrowserWindow({ width: 600, height: 600, title: "map", frame: false })
+    mapWindow = new BrowserWindow({ width: 600, height: 600, title: "map"})
+    mapWindow.setMenuBarVisibility(false)
     mapWindow.loadFile('map.html')
     // Handle garbage collection
     mapWindow.on('close', function () {
@@ -135,9 +144,14 @@ function createMapWindow() {
 };
 
 //Catches settingsAdd and changes the settings -> closes window
-ipcMain.on('settingsSave', function (e, msg) {
-    console.log(msg)
+ipcMain.on('settingsSave', function (e, type,range,frequency) {
+    console.log("type" + type);
+    console.log("range" + range);
+    console.log("frequency" + frequency);
     settingsWindow.close();
+    setUrl(type,range);
+    store.set("frequency",frequency);
+    mainWindow.webContents.send("update");
 
 });
 ipcMain.on('closeMap', function (e) {
@@ -147,13 +161,13 @@ ipcMain.on('closeMap', function (e) {
 });
 
 ipcMain.on('openMap', function (e, msg) {
+
     var arrayOfStrings = msg.split(",")
     createMapWindow();
-    console.log(msg);
+
     mapWindow.webContents.on('did-finish-load', function () {
 
         mapWindow.webContents.send('sendCoords', arrayOfStrings);
-        console.log("sent")
     });
 
 
@@ -173,3 +187,69 @@ ipcMain.on('openNotification', function (e, msg) {
 
 });
 
+function setUrl(type,range){
+
+    switch(true){
+        case type == "Significant" && range == "hour":
+        store.set("url","https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_hour.geojson");
+        break;
+        case type == "Significant" && range == "day":
+        store.set("url","https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_day.geojson");
+        break;
+        case type == "Significant" && range == "7days":
+        store.set("url","https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson");
+        break;
+        case type == "Significant" && range == "30days":
+        store.set("url","https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson");
+        break;
+        case type == "4.5" && range == "hour":
+        store.set("url","https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_hour.geojson");
+        break;
+        case type == "4.5" && range == "day":
+        store.set("url","https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_day.geojson");
+        break;
+        case type == "4.5" && range == "7days":
+        store.set("url","https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson");
+        break;
+        case type == "4.5" && range == "30days":
+        store.set("url","https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson");
+        break;
+        case type == "2.5" && range == "hour":
+        store.set("url","https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_hour.geojson");
+        break;
+        case type == "2.5" && range == "day":
+        store.set("url","https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson");
+        break;
+        case type == "2.5" && range == "7days":
+        store.set("url","https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson");
+        break;
+        case type == "2.5" && range == "30days":
+        store.set("url","https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson");
+        break;
+        case type == "1" && range == "hour":
+        store.set("url","https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_hour.geojson");
+        break;
+        case type == "1" && range == "day":
+        store.set("url","https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_day.geojson");
+        break;
+        case type == "1" && range == "7days":
+        store.set("url","https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson");
+        break;
+        case type == "1" && range == "30days":
+        store.set("url","https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_month.geojson");
+        break;
+        case type == "All" && range == "hour":
+        store.set("url","https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson");
+        break;
+        case type == "All" && range == "day":
+        store.set("url","https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson");
+        break;
+        case type == "All" && range == "7days":
+        store.set("url","https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson");
+        break;
+        case type == "All" && range == "30days":
+        store.set("url","https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson");
+        break;
+    
+       }
+}
